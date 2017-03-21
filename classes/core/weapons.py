d8d58ea12aedbf3ddb728321b64e7bb681b24d9d
Duckerino303 <dakashi303@hotmail.com>
2017-03-21 22:07:06 +0100

    minor changes

--- classes/core/weapons.py
+++ classes/core/weapons.py
@@ -1,6 +1,9 @@
 class Weapon:
-    def __init__(self, dmg=1, speed=50, img=None, sound=None):
+    def __init__(self, dmg=1, speed=50, img=None, sound=None, maxBullets = 5):
         self.dmg = dmg
         self.speed = speed
         self.img = img
         self.sound = sound
+        self.maxBullets = maxBullets
+
+    def drawBullet(self, x, y):

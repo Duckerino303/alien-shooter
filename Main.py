1829ddbfbba82ab3cf766875de0a4aca0c1fa442
Duckerino303 <dakashi303@hotmail.com>
2017-03-21 20:55:32 +0100

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,82 @@
+import pygame
+from pygame.locals import *
+import time
+import sys
+import os
+import random
+
+pygame.init()
+
+WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 800
+
+WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Alien Shooter')
+
+# R G B
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+clock = pygame.time.Clock()
+
+shipImg = pygame.image.load('ship.png')
+
+shipWidth = 166
+shipHeight = 309
+
+def img(x, y):
+    WINDOW.blit(shipImg, (x, y))
+
+
+def gameLoop():
+    x = (WINDOW_WIDTH * 0.45)
+    y = (WINDOW_HEIGHT * 0.45)
+
+    xChange = 0
+    yChange = 0
+
+    shipSpeed = 5
+
+    gameExit = False
+
+    while not gameExit:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit(0)
+
+            if event.type == KEYDOWN:
+                if event.key == K_LEFT:
+                    xChange = -shipSpeed
+                elif event.key == K_RIGHT:
+                    xChange = shipSpeed
+                elif event.key == K_UP:
+                    yChange = -shipSpeed
+                elif event.key == K_DOWN:
+                    yChange = shipSpeed
+
+            if event.type == KEYUP:
+                if event.key == K_LEFT or event.key == K_RIGHT:
+                    xChange = 0
+                elif event.key == K_UP or event.key == K_DOWN:
+                    yChange = 0
+
+        x += xChange
+        y += yChange
+
+        WINDOW.fill(black)
+        img(x, y)
+
+        if x > WINDOW_WIDTH - shipWidth or x < 0:
+            xChange = 0
+        if y > WINDOW_HEIGHT - shipHeight or y < 0:
+            yChange = 0
+
+        pygame.display.update()
+        clock.tick(60)
+
+gameLoop()
+pygame.quit()
+quit()

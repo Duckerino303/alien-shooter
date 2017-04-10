2b5fdf575d06bdd99d13e4bec759aca78aeea694
TomaszBochenek <tomasz.bochenek12@gmail.com>
2017-04-10 18:10:38 +0200

    bonuses feature alpha

--- classes/core/drawer.py
+++ classes/core/drawer.py
@@ -3,13 +3,10 @@ import classes.core.settings as settings
 import random
 
 def start():
-    threading.Thread(target=draw_background).start()
-    threading.Thread(target=draw_enemies).start()
+    threading.Thread(target=draw).start()
 
-def draw_background():
+def draw():
     settings.WINDOW.blit(settings.BACKGROUND_IMG, (0, 0))
-
-def draw_enemies():
     for enemy in settings.LIST_OF_ENEMIES:
         settings.WINDOW.blit(enemy.img, (enemy.x, enemy.y))
         if random.randint(1, 1000) < 2:
